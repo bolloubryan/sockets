2b75c4c72ffb09ad3bcb4fef0a44bf445b1f50aa
Bryan Bollou <bolloubryan@gmail.com>
2021-02-09 22:13:30 -0500

    working on the joins, really need a fundemental understanding of sockets

--- moresockets/socket-io-server/app.js
+++ moresockets/socket-io-server/app.js
@@ -108,11 +108,20 @@ const io = require('socket.io')(server, {
 });
 
 io.on("connection", (socket) => {
+  console.log(socket.id)
   console.log("New client connected");
   socket.on("disconnect", () => {
     console.log("Client disconnected");
   });
+
+  socket.on("joinroom", (socket) => {
+    console.log("join")
+    //console.log(socket)
+    //console.log(socket.id)
+  });
 });
+
+
 
 // const getApiAndEmit = socket => {
 //   const response = new Date();
